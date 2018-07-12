b5040481ba92404a944ac53e369a1001440bc5cc
Akihito Morita <moriakijp@gmail.com>
2018-07-13 02:01:08 +0900

    Update

--- assets/Main/State.pde
+++ assets/Main/State.pde
@@ -2,18 +2,18 @@ abstract class State {
   float t_point, t_state;
   int strokeweight;
   StringList inventory;
-  
+
   State() {
     t_point = millis();
   }
-  
+
   State load() {
     t_state = ( millis() - t_point ) / 1000.0;
     update();
     display();
     return next();
   }
-  
+
   int[] n_rand(int number){
   IntList nums = new IntList(number);
   for (int i = 0; i < number; i++){
@@ -23,7 +23,7 @@ abstract class State {
   int[] result = nums.array();
   return result;
 }
-  
+
   abstract void update();
   abstract void display();
   abstract State next();
---
+++ assets/main.js
@@ -0,0 +1,10 @@
+// const drawGame = () => {
+//     const obj = document.getElementById('game')
+//     const canvas = document.createElement('canvas')
+//     .id = 'canvas';
+//     .data-processing-source='assets/Main/Zoog_Bouncing.pde assets/Main/State.pde assets/Main/State_Class_Hanekaese.pde assets/Main/State_Game_Yokero.pde assets/Main/State_Game_Nerae.pde assets/Main/Board.pde assets/Main/Human.pde assets/Main/State_Title.pde assets/Main/Count.pde assets/Main/Sight.pde assets/Main/Zoog.pde assets/Main/State_End_Yo';
+
+//     obj.appendChild(canvas);
+// }
+
+// drawGame();
